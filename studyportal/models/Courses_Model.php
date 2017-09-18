d490e930e7b95a37645557a2c5e939e9f64c6e19
Manik Mahajan <manikmahajan9090@gmail.com>
2017-09-18 21:02:30 +0530

    changes for review system

--- studyportal/models/Courses_Model.php
+++ studyportal/models/Courses_Model.php
@@ -49,6 +49,45 @@ class Courses_Model extends CI_Model
 		End Function used by jb_helper to check if user already enrolled for batch
 	*********************************/	
 	
+	/********************************
+		Start Function to fetch ratings based on course id
+	*********************************/
+	function checkUserRatings($course_id) {
+		$this->db->select("*");
+		$this->db->from("jb_rating");
+		$this->db->where("course_id",$course_id);
+		$this->db->where("status","1");
+		$query = $this->db->get();
+		if($query->num_rows()>0) {
+			$response = $query->result();
+		} else {
+			$response = array();
+		}
+		return $response;
+	}
+	/********************************
+		End Function to fetch ratings based on course id
+	*********************************/
+	
+	/********************************
+		Start Function to check if user enrolled in particular course
+	*********************************/
+	function userEnrollStatus($course_id,$user_id) {
+		$this->db->select("*");
+		$this->db->from("user_enrolled_batches");
+		$this->db->where("course_id",$course_id);
+		$this->db->where("user_id",$user_id);
+		$query = $this->db->get();
+		if($query->num_rows()>0) {
+			$response = array("UserLoggedIn"=>"1","userEnrollStatus"=>"1","course_id"=>$course_id,"user_id"=>$user_id);
+		} else {
+			$response = array("UserLoggedIn"=>"1","userEnrollStatus"=>"0","course_id"=>$course_id,"user_id"=>$user_id);
+		}
+		return $response;
+	}
+	/********************************
+		End Function to check if user enrolled in particular course
+	*********************************/
 }
 	
 ?>
